d2eacf5145fb70e2ee39bc83fdc330f524e629e3
Lukas Wunner <lukas@wunner.de>
2019-03-20 15:02:00 +0100

    net: ks8851: Reassert reset pin if chip ID check fails
    [ Upstream commit 761cfa979a0c177d6c2d93ef5585cd79ae49a7d5 ]
    
    Commit 73fdeb82e963 ("net: ks8851: Add optional vdd_io regulator and
    reset gpio") amended the ks8851 driver to briefly assert the chip's
    reset pin on probe. It also amended the probe routine's error path to
    reassert the reset pin if a subsequent initialization step fails.
    
    However the commit misplaced reassertion of the reset pin in the error
    path such that it is not performed if the check of the Chip ID and
    Enable Register (CIDER) fails. The error path is therefore slightly
    asymmetrical to the probe routine's body. Fix it.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Cc: Frank Pavlic <f.pavlic@kunbus.de>
    Cc: Stephen Boyd <sboyd@codeaurora.org>
    Cc: Nishanth Menon <nm@ti.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin (Microsoft) <sashal@kernel.org>

--- drivers/net/ethernet/micrel/ks8851.c
+++ drivers/net/ethernet/micrel/ks8851.c
@@ -1807,9 +1807,11 @@ err_netdev:
 	free_irq(ndev->irq, ks);
 
 err_irq:
+err_id:
 	if (gpio_is_valid(gpio))
 		gpio_set_value(gpio, 0);
-err_id:
+	regulator_disable(ks->vdd_reg);
+	regulator_disable(ks->vdd_io);
 err_gpio:
 	free_netdev(ndev);
 	return ret;
